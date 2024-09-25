6323bf63fdad0743c27aea1ee855318e62cad4fb
James Griffin <42494284+Comcenn@users.noreply.github.com>
2024-09-25 23:10:21 +0100

    BoardCrawler WIP

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+
+type Board = [[bool; 5]; 6];
+
+const INITIAL_BOARD: Board = [
+    [false, false, false, false, false],
+    [false, true, false, false, false],
+    [false, false, false, false, false],
+    [false, false, true, true, false],
+    [false, false, false, false, false],
+    [false, false, false, false, false],
+];
+
+struct BoardCrawler {
+    x: i32,
+    y: i32,
+    board: Board
+}
+
+impl BoardCrawler {
+    fn new(initial_board: Board) -> Self{
+        Self {
+            x:0,
+            y:0,
+            board:initial_board
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let crawler = BoardCrawler::new(INITIAL_BOARD);
 }
